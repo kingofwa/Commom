068f1213e1d4e68c4416f8d5008f7415d6bbc393
le nguyenvan <lenguyenvan09@gmail.com>
2021-03-05 11:17:39 +0700

    comment Thay đổi

---
+++ JavaScriptBasic/main.js
@@ -0,0 +1,19 @@
+// function GiaiThua(number){
+//     if(number > 1){
+//        return number * GiaiThua(number-1);
+//     }
+//     return 1;
+// }
+// console.log(GiaiThua(4))
+
+var array = [
+    "nguyen van le1",
+    "nguyen van le2",
+    "nguyen van le3","nguyen van le3","nguyen van le6","nguyen van le7",
+];
+
+var MyArray = array.length;
+for(var i=0; i<MyArray; i++){
+    console.log(array[i])
+}
+ 
---
+++ jqueryex/selector.js
@@ -0,0 +1,8 @@
+
+function Phuongtrinh(){
+  var a = parseInt( document.getElementById("Numbera").value);
+  var b = parseInt( document.getElementById("Numberb").value);
+  var c = parseInt(document.getElementById("Numberc").value);
+ 
+  
+}
